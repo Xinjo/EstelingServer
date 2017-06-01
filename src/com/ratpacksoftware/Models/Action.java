b2a7cb8543af29cd79cf2d6d97afb26f2a9f9ab7
Michel de Nijs <micheldenijs@mail.com>
2017-06-01 11:01:49 +0200

    MANY A FIX

--- src/com/ratpacksoftware/Models/Action.java
+++
@@ -1,9 +0,0 @@
-package com.ratpacksoftware.Models;
-
-/**
- * Created by stijn on 31-5-2017.
- */
-public class Action {
-    public int id;
-    public String description;
-}
--- src/com/ratpacksoftware/UI/ServerGui.java
+++
@@ -1,9 +0,0 @@
-package com.ratpacksoftware.UI;
-
-import javax.swing.*;
-
-/**
- * Created by Michel on 18-5-2017.
- */
-public class ServerGui extends JPanel{
-}
